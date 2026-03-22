f9236058b20933a3ae9ae812212a2ba9858f841c
Gabrielgalhar <galhardonefelipegabriel@gmail.com>
2026-03-21 22:46:30 -0300

    service criada

--- src/main/java/com/example/Livro/service/ProdutoService.java
+++ src/main/java/com/example/Livro/service/ProdutoService.java
@@ -1,4 +1,34 @@
 package com.example.Livro.service;
 
+import com.example.Livro.entity.Produto;
+import com.example.Livro.repository.ProdutoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ProdutoService {
+
+    private final ProdutoRepository produtoRepository;
+
+    public ProdutoService(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
+    public Produto salvar(Produto produto) {
+        return produtoRepository.save(produto);
+    }
+
+    public List<Produto> listarTodos() {
+        return produtoRepository.findAll();
+    }
+
+    public Optional<Produto> buscarPorId(Long id) {
+        return produtoRepository.findById(id);
+    }
+
+    public void deletarPorId(Long id) {
+        produtoRepository.deleteById(id);
+    }
 }
