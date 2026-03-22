4910e06f3ba52c3be82ad9f952a731cb7d19806a
Gabrielgalhar <galhardonefelipegabriel@gmail.com>
2026-03-21 23:02:28 -0300

    repositorio e entidade criadas ex 3

--- src/main/java/com/example/Livro/repository/AutorRepository.java
+++ src/main/java/com/example/Livro/repository/AutorRepository.java
@@ -1,4 +1,7 @@
 package com.example.Livro.repository;
 
-public interface AutorRepository {
+import com.example.Livro.entity.Autor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AutorRepository extends JpaRepository<Autor, Long> {
 }
