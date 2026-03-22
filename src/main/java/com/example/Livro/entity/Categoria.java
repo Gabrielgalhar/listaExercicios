e208daa767f7a8d5f93743010cc92002947dc6f3
Gabrielgalhar <galhardonefelipegabriel@gmail.com>
2026-03-21 23:12:26 -0300

    criada entidade e repositorio ex05

--- src/main/java/com/example/Livro/entity/Categoria.java
+++ src/main/java/com/example/Livro/entity/Categoria.java
@@ -1,4 +1,51 @@
 package com.example.Livro.entity;
 
+import jakarta.persistence.*;
+
+@Entity(name = "Categoria")
+@Table(name = "tb_categoria")
 public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nm_categoria")
+    private String nome;
+
+    @Column(name = "ds_categoria")
+    private String descricao;
+
+    public Categoria() {
+    }
+
+    public Categoria(Long id, String nome, String descricao) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 }
--- src/main/java/com/example/Livro/repository/CategoriaRepository.java
+++ src/main/java/com/example/Livro/repository/CategoriaRepository.java
@@ -1,4 +1,7 @@
 package com.example.Livro.repository;
 
-public interface CategoriaRepository {
+import com.example.Livro.entity.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 }
