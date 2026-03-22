e668cc18064d7e0f42cbb9dcf695af8bdc7a2581
Gabrielgalhar <galhardonefelipegabriel@gmail.com>
2026-03-21 22:55:35 -0300

    service criada

--- src/main/java/com/example/Livro/service/PedidoService.java
+++ src/main/java/com/example/Livro/service/PedidoService.java
@@ -1,4 +1,34 @@
 package com.example.Livro.service;
 
+import com.example.Livro.entity.Pedido;
+import com.example.Livro.repository.PedidoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class PedidoService {
+
+    private final PedidoRepository pedidoRepository;
+
+    public PedidoService(PedidoRepository pedidoRepository) {
+        this.pedidoRepository = pedidoRepository;
+    }
+
+    public Pedido salvar(Pedido pedido) {
+        return pedidoRepository.save(pedido);
+    }
+
+    public List<Pedido> listarTodos() {
+        return pedidoRepository.findAll();
+    }
+
+    public Optional<Pedido> buscarPorId(Long id) {
+        return pedidoRepository.findById(id);
+    }
+
+    public void deletarPorId(Long id) {
+        pedidoRepository.deleteById(id);
+    }
 }
