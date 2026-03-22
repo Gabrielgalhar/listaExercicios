aeb2d0f098413dab1fa0ece46728e28a8552463c
Gabrielgalhar <galhardonefelipegabriel@gmail.com>
2026-03-21 23:29:56 -0300

    Service ex06 criado

--- src/main/java/com/example/Livro/service/ClienteService.java
+++ src/main/java/com/example/Livro/service/ClienteService.java
@@ -1,4 +1,34 @@
 package com.example.Livro.service;
 
+import com.example.Livro.entity.Cliente;
+import com.example.Livro.repository.ClienteRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ClienteService {
+
+    private final ClienteRepository clienteRepository;
+
+    public ClienteService(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
+
+    public Cliente salvar(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    public List<Cliente> listarTodos() {
+        return clienteRepository.findAll();
+    }
+
+    public Optional<Cliente> buscarPorId(Long id) {
+        return clienteRepository.findById(id);
+    }
+
+    public void deletarPorId(Long id) {
+        clienteRepository.deleteById(id);
+    }
 }
